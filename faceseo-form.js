d63f4002619b1687972e75b68f21400ef76a1fa7
noithatbluetphcm <52003621+noithatbluetphcm@users.noreply.github.com>
2019-06-21 21:29:13 +0700

    Update faceseo-form.js

--- faceseo-form.js
+++ faceseo-form.js
@@ -140,7 +140,7 @@ jQuery("#myModalthanh").html(formdiv);
 function thanhxet(){
 
 if(submittedform) {
-	window.location="https://hocvienfaceseo.com";
+	window.location="http://thicongnoithathcm.net/";
 	
 	 if(document.cookie.indexOf("adf") == -1)
                    {
